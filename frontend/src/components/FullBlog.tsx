6df31ea547c711a601aa1bc6603fd616b75ed3db
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-10-26 22:05:28 +0530

    frontend bugs fixed1

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -5,13 +5,13 @@ import { Phrases } from "./Phrases";
 const FullBlog = ({ blog }:{blog:Blog} ) => {
   return <div className="p-8">
     {blog &&
-    <div className="flex justify-between items-center p-4 border-2">
-        <div className="max-w-4xl border">
+    <div className="flex justify-between items-center p-4 ">
+        <div className="max-w-4xl ">
             <div className="font-bold text-3xl">{blog.title}</div>
             <div className="my-2 text-slate-800 font-medium font-serif">{blog.content}</div>
             <div className="font-medium ">{`Posted by ${blog.author.name || "Anonymous"}`}</div>
         </div>
-        <div className="px-3 max-w-lg border">
+        <div className="px-3 max-w-lg ">
             <div className="flex items-center gap-2 ">
             <div>
                 <Avatar size="small" name={`${blog.author.name || "Anonymous"}`}/>
