14eefe078a330f70e9829d6ddf124da97aebb884
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-10-27 11:55:17 +0530

    fixing frontend5

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -21,7 +21,7 @@ const BlogCard = ( {id,title,content,name,publishedDate,imageLink}: BlogCardProp
                 <div className="font-medium text-sm text-[#DEB68C]"style={{ wordSpacing: '-2px' }}>{publishedDate}</div>
             </div>
             <div>
-                <div className="font-bold text-2xl max-w-3xl border border-red-300 ">{title.slice(0,50)}...</div>
+                <div className="font-bold text-2xl max-w-3x ">{title.slice(0,50)}...</div>
             </div>
             <div>
                 <div className="text-[#3c2309] font-serif">{content.slice(0,200)}...</div>
